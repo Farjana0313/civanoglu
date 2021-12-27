7f9b63fe23a1c781086ff1251d9549bc9c5608c9
Farjana0313 <farjanaakter178@gmail.com>
2021-12-27 11:05:20 +0600

    Initial commit

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,18 @@
+<div class="fixed w-full py-4 px-12 flex justify-between items-center z-30">
+    <div class="min-w-max">
+        <img src="/img/download.png" width="100" alt="">
+    </div>
+    <div class="w-full">
+        <ul class="flex justify-center">
+            <li><a href="" class="inline-block p-4 text-white">Land</a></li>
+            <li><a href="" class="inline-block p-4 text-white">Villa</a></li>
+            <li><a href="" class="inline-block p-4 text-white">Appartment</a></li>
+            <li><a href="" class="inline-block p-4 text-white">About Us</a></li>
+            <li><a href="" class="inline-block p-4 text-white">Contact Us</a></li>
+        </ul>
+    </div>
+    <div class="min-w-max text-3xl">
+        <a href="" class="text-white">🇺🇸</a>
+        <a href="" class="text-white">🇧🇩</a>
+    </div>
+</div>
